278f42fd7b7c0053ae8a086c94a1980c013a89a5
Agustin Romero <agus_artik@hotmail.com.ar>
2023-10-13 21:18:02 -0300

    CRUD Proveedores

---
+++ C_Presentacion/CRUD_Proveedores.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccesoLogin.C_Presentacion
+{
+    public partial class CRUD_Proveedores : Form
+    {
+        public CRUD_Proveedores()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
